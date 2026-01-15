fbdfdd34197629cc383e678490a959afa17161ab
Sasha22D <sasha.daniel2006@gmail.com>
2026-01-15 16:35:57 +0100

    cleaned int main()

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "so_long.h"
-#include <stdio.h>
 
 int	main()
 {
@@ -9,27 +8,12 @@ int	main()
 
 	fd = open("map.ber", O_RDONLY);
 	gameStruct = malloc(sizeof(t_game));
-	map = init_matrix(fd, gameStruct);
-	if (map == NULL)
+	if (map_checker(map, fd, gameStruct) == -1)
 	{
 		destroy_map(map);
 		free(gameStruct);
 		close(fd);
-		return (-1);
-	}
-	fill_matrix(map);
-	if (check_walls(map) || check_exit_and_player(map, gameStruct)
-		|| check_collectibles(map) < 1)
-	{
-		free(gameStruct);
-		destroy_map(map);
-		close(fd);
-		return (0);
-	}
-	else
-	{
-		int result = check_path(map, (gameStruct)->player_x, (gameStruct)->player_y, check_collectibles(map));
-		printf("%d\n", result);
+		exit(EXIT_FAILURE);
 	}
 	destroy_map(map);
 	free(gameStruct);
