f24e155628e815fd6f78b3e0b4910b3c51429b61
Sasha Daniel <sadaniel@f4r6s15.clusters.42paris.fr>
2026-01-16 11:48:04 +0100

    map checker now returns the map

--- map/map_checker.c
+++ map/map_checker.c
@@ -1,19 +1,18 @@
 #include "so_long.h"
 
-int	map_checker(char **map, int fd, t_game *gameStruct)
+char	*map_checker(char **map, int fd, t_game *gameStruct)
 {
 	map = init_matrix(fd, gameStruct);
 	if (map == NULL)
-		return (destroy_map(map), error_printer('m'), -1);
+		return (destroy_map(map), error_printer('m'), NULL);
 	fill_matrix(map);
 	if (check_walls(map) == -1)
-		return (destroy_map(map), error_printer('w'), -1);
+		return (destroy_map(map), error_printer('w'), NULL);
 	if (check_exit_and_player(map, gameStruct) == -1)
-		return (destroy_map(map), error_printer('e'), -1);
+		return (destroy_map(map), error_printer('e'), NULL);
 	if (check_collectibles(map) == 0)
-		return (destroy_map(map), error_printer('c'), -1);
+		return (destroy_map(map), error_printer('c'), NULL);
 	if (check_path(map, gameStruct->player_x, gameStruct->player_y, check_collectibles(map), gameStruct) == -1)
-		return (destroy_map(map), error_printer('p'), -1);
-	destroy_map(map);
-	return (0);
+		return (destroy_map(map), error_printer('p'), NULL);
+	return (map);
 }
