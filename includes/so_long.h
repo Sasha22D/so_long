94e8c995889422b35427bf077043abe4145cc210
Sasha Daniel <sadaniel@f4r6s15.clusters.42paris.fr>
2026-01-16 11:49:58 +0100

    fixed so_long.h and changed main

--- includes/so_long.h
+++ includes/so_long.h
@@ -13,14 +13,14 @@ typedef struct	s_game
 
 // =========MAP==========
 char	**init_matrix(int fd, t_game *gameStruct);
-void	fill_matrix(char ***map);
+void	fill_matrix(char **map);
 // MAP CHECKS
 int	check_walls(char **map);
 int	check_exit_and_player(char **map, t_game *gameStruct);
 int	check_collectibles(char **map);
 int	check_path(char **map, int x, int y, int count, t_game *gameStruct);
 // MAP CHECKER
-int	map_checker(char ***map, int fd, t_game *gameStruct);
+int	map_checker(char **map, int fd, t_game *gameStruct);
 // UTILS FUNCTIONS
 int	lenstr(char *str);
 void	destroy_map(char **map);
--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include "so_long.h"
 
-int	main()
+int	main(int ac, char **av)
 {
 	int	fd;
 	char	**map = NULL;
 	t_game	*gameStruct;
 
-	fd = open("map.ber", O_RDONLY);
-	gameStruct = malloc(sizeof(t_game));
-	if (map_checker(map, fd, gameStruct) == -1)
+	if (ac == 2)
 	{
+		fd = open(av[1], O_RDONLY);
+		if (fd < 0)
+			// PRINT ERROR FD
+		gameStruct = malloc(sizeof(t_game));
+		if (!gameStruct)
+			exit(EXIT_FAILURE);
+		map = map_checker(map, fd, gameStruct);
+		if (map == NULL)
+			error_free(map, fd, gameStruct);
 		free(gameStruct);
-		close(fd);
-		exit(EXIT_FAILURE);
+		destroy_map(map);
 	}
-	free(gameStruct);
-	close(fd);
 }
+
