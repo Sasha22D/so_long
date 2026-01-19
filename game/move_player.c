a2a2bffc193ed731a7a842602b47b2f3914ba841
Sasha Daniel <sadaniel@f4r2s14.clusters.42paris.fr>
2026-01-19 17:22:00 +0100

    player_x was only updating locally

--- game/move_player.c
+++ game/move_player.c
@@ -17,8 +17,8 @@ void	move_up(t_game *gameStruct)
 		if (map[x - 1][y] == 'C')
 			gameStruct->count_collectibles--;
 		map[x][y] = '0';
-		x--;
-		map[x][y] = 'P';
+		gameStruct->player_x = x--;
+		map[gameStruct->player_x][y] = 'P';
 		render_player_collectibles_exit(gameStruct);
 	}
 	// if (map[x][y + 1] == 'E')
